a85c2d0d96466ad7dad7820b55dad56d28c78a30
Ryan Hendrickson <8751750+rynhndrcksn@users.noreply.github.com>
2025-11-03 18:10:22 -0800

    feat: add rector
    Requires installing phpunit/phpunit.

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 $finder = new PhpCsFixer\Finder()
     ->in([
+        __DIR__.'/bin',
         __DIR__.'/src',
         __DIR__.'/tests',
     ])
---
+++ rector.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+// Docs: https://getrector.com
+use Rector\Exception\Configuration\InvalidConfigurationException;
+use Rector\Symfony\Bridge\Symfony\Routing\SymfonyRoutesProvider;
+use Rector\Symfony\Contract\Bridge\Symfony\Routing\SymfonyRoutesProviderInterface;
+use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
+
+try {
+    return Rector\Config\RectorConfig::configure()
+        ->withPaths([
+            __DIR__.'/bin',
+            __DIR__.'/src',
+            __DIR__.'/tests',
+        ])
+        ->withSymfonyContainerPhp(__DIR__.'/tests/symfony-container.php')
+        ->registerService(SymfonyRoutesProvider::class, SymfonyRoutesProviderInterface::class)
+        ->withPhpSets()
+        ->withPreparedSets(
+            deadCode: true,
+            codeQuality: true,
+            codingStyle: true,
+            typeDeclarations: true,
+            typeDeclarationDocblocks: true,
+            privatization: true,
+            naming: true,
+            instanceOf: true,
+            earlyReturn: true,
+            phpunitCodeQuality: true,
+            doctrineCodeQuality: true,
+            symfonyCodeQuality: true,
+            symfonyConfigs: true,
+        )
+        ->withComposerBased(
+            twig: true,
+            doctrine: true,
+            phpunit: true,
+            symfony: true
+        )
+        ->withRules([
+            DeclareStrictTypesRector::class,
+        ])
+    ;
+} catch (InvalidConfigurationException $exception) {
+    dd($exception);
+}
