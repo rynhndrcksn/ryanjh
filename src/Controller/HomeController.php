b62e3cebc7c9023a0fce92ba7b04a8ee0f31f6e5
Ryan Hendrickson <8751750+rynhndrcksn@users.noreply.github.com>
2026-01-15 19:55:18 -0800

    fix: streamline years of experience on home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,8 +13,6 @@ final class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'years_of_experience' => date('Y') - 2022,
-        ]);
+        return $this->render('home/index.html.twig');
     }
 }
